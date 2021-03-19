44c5b02d6798151f9632bdf849c6958826ee771d
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-03-19 07:40:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -10,5 +10,9 @@ window.onload = function(){
   canvasContext.fillRect(0,0,canvas.clientWidth,canvas.height);
 
   canvasContext.fillStyle = 'red';
-  canvasContext.fillRect(100,200,50,25);
+  canvasContext.fillRect(200,200,50,25);
+
+  canvasContext.fillStyle = 'white';
+  canvasContext.fillRect(300,200,200,200);
+  
 }
